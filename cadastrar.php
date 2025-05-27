ce5602dc21195c081de78a44e38d21f673e537a9
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2025-05-26 21:38:53 -0300

    Add files via upload

---
+++ cadastrar.php
@@ -0,0 +1,20 @@
+<?php
+include 'pagina.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$cpf = $_POST['cpf'];
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+	$telefone = $_POST['telefone'];
+
+    $sql = "INSERT INTO pessoas (cpf,nome, email,telefone) 
+	VALUES ('$cpf','$nome', '$email','$telefone');";
+
+    if (mysqli_query($conn, $sql)) {
+        header("Location: index.php"); // Redireciona de volta para a página principal
+        exit();
+    } else {
+        echo "Erro ao cadastrar: " . mysqli_error($conn);
+    }
+	}
+mysqli_close($conn);
+?>
---
+++ pagina.php
@@ -0,0 +1,19 @@
+<?php
+
+$servidor = "localhost"; // Onde o MySQL está rodando
+$usuario = "root";     // Seu usuário do MySQL
+$senha = "";           // Sua senha do MySQL (deixe em branco se não tiver)
+$dbname = "banco";   // Nome do banco de dados que criamos
+
+// Conexão com o banco de dados
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+// Verifica a conexão
+if (!$conn) {
+    die("Falha na conexão: " . mysqli_connect_error());
+}
+ echo "Conexão bem-sucedida!"; // Apenas para testar a conexão, remova em produção
+
+echo "Está certo!";
+
+?>
